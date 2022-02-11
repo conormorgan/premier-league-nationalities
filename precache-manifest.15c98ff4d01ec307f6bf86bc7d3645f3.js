d3f5cf481d2f232cf669915666131198993bb740
conormorgan <morgan.fourteen@hotmail.co.uk>
2022-02-11 21:58:07 +0000

    Updates

---
+++ precache-manifest.15c98ff4d01ec307f6bf86bc7d3645f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a8d0fe369d243e013806b911656b22c",
+    "url": "/premier-league-nationalities/index.html"
+  },
+  {
+    "revision": "5a6b8b54c078ae91a95b",
+    "url": "/premier-league-nationalities/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "8dfb469e682db057a54f",
+    "url": "/premier-league-nationalities/static/js/2.e56f3dad.chunk.js"
+  },
+  {
+    "revision": "914b429ab2df8f9797abeb28d7ff65b7",
+    "url": "/premier-league-nationalities/static/js/2.e56f3dad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a6b8b54c078ae91a95b",
+    "url": "/premier-league-nationalities/static/js/main.9eab5745.chunk.js"
+  },
+  {
+    "revision": "9101d619e1bd05cf580c",
+    "url": "/premier-league-nationalities/static/js/runtime-main.5c081f1e.js"
+  },
+  {
+    "revision": "0e30f6314898c8711b798b9265a6db9c",
+    "url": "/premier-league-nationalities/static/media/top20_countries.0e30f631.csv"
+  },
+  {
+    "revision": "47d6ad6802bf531c29247da27c5848b3",
+    "url": "/premier-league-nationalities/static/media/top20_no_eng.47d6ad68.csv"
+  }
+]);
